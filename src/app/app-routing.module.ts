e9d60e532ea75c20b6b3120f949452726b27fc97
Thomas <thomas.donnay@live.fr>
2020-07-06 17:36:57 +0200

    feature ok manque le back

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Routes, RouterModule } from "@angular/router";
 import { ListArticleFavoriteComponent } from "./list-article-favorite/list-article-favorite.component";
+import {ArticleConsultationComponent} from "./article-consultation/article-consultation.component";
 
 const routes: Routes = [
   { path: "liste", component: ListArticleFavoriteComponent },
+  { path: "", component: ArticleConsultationComponent},
+
 ];
 @NgModule({
   declarations: [],
