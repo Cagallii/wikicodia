3d9216cbbd2da005373bd3cdc6134a8c5eda3c03
Thomas <thomas.donnay@live.fr>
2020-08-15 13:16:38 +0200

    article creation
    article creation

---
+++ src/app/services/article.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import Article from '../model/Article';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+
+  constructor(private http: HttpClient) { }
+
+  private baseUrl = 'http://localhost:8080/articles/';
+
+
+  create(article: Article): Observable<Object> {
+    return this.http.post(this.baseUrl+ "/creation", article);
+  }
+
+}
